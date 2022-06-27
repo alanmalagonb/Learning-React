0eac6532290608a850599d63c570c69e54bdf883
Alan Malagon <alanmalagonb@gmail.com>
2022-06-27 14:24:47 -0500

    React Complete Guide
    React Basic & Working with Components
    React State & Working with Events

---
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,43 @@
+// Importing state
+import React, { useState } from 'react';
+
+// Considering css file
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+// Custom Component
+
+// Component : Just a JS function
+// Convention name function as js file, must start with uppercase
+
+// 1 parameter props : atributes xml
+const ExpenseItem = (props) => {
+  
+  // Hook: called inside react compoenent
+  // First Array element: props value
+  // Second element: updating function
+  const [title, setTitle] = useState(props.title);
+
+  // Event
+  // function clickHandler(){}
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
+
+  // Return JSX
+  // Only 1 root element allowed
+  // className instead of class
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+// Export to use in other files
+export default ExpenseItem;
---
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+
+    // Get data from child
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        //console.log(expenseData);
+        props.onAddExpense(expenseData);
+    }
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
+    </div>
+  );
+};
+
+export default NewExpense;
